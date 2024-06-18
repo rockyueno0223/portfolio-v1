96e1adb7246a846787b3653f3926189c6db0109c
Hiroki Ueno <rockyueno@gmail.com>
2024-06-18 13:43:41 -0700

    Update Button Link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,11 @@ const Button = ({title, pass}) => {
   return (
     <div>
       <div className='button'>
-        <Link to={pass}>
+        <Link to={pass}
+          onClick={() => {
+            window.scroll(0, 0);
+          }}
+        >
           <p>{title}</p>
         </Link>
       </div>
