983b9caa06a97b6d549cb2dbe7c627d9dc502954
Hiroki Ueno <rockyueno@gmail.com>
2024-06-19 14:11:38 -0700

    Update Project Detail style

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
-const ProjectDetail = ({img, text}) => {
+const ProjectDetail = ({ id, name, img, url, skills, text }) => {
+  const ProjectDetailContentClass = (({ id }) => {
+    let classText = "project-detail-content "
+    if (id % 2 !== 0) {
+      classText += "odd"
+    } else if (id % 2 === 0) {
+      classText += "even"
+    }
+    return classText
+  })
+
+  const skillText = skills.reduce((result, curr) => {
+    return `${result} / ${curr}`
+  })
+
   return (
     <div className='project-detail'>
       <div className='project-detail-img'>
-        <img src={img} alt="" />
+        <a href={url} target="_blank" rel="noreferrer">
+            <img src={img} alt={name} />
+          </a>
       </div>
-      <div className='project-detail-text'>
-        {text}
+      <div className={ProjectDetailContentClass({id})}>
+        <h3 className='title'>{name}</h3>
+        <p className='skills'>{skillText}</p>
+        <p className='text'>{text}</p>
       </div>
     </div>
   )
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -7,20 +7,24 @@ import Work1Img from "../img/sample/sample-work1.png"
 import Work2Img from "../img/sample/sample-work2.png"
 import Work3Img from "../img/sample/sample-work3.png"
 
-const workObj = [
-  { img: Work1Img, text: "This is an explanation of work1. Explain about this work such as when, how I made it." },
-  { img: Work2Img, text: "This is an explanation of work2. Explain about this work such as when, how I made it." },
-  { img: Work3Img, text: "This is an explanation of work3. Explain about this work such as when, how I made it." }
+const projects = [
+  { id: 1, name: "Work1", img: Work1Img, url:"#", skills:["HTML", "CSS"], text: "This is an explanation of work1. Explain about this work such as when, how I made it." },
+  { id: 2, name: "Work2", img: Work2Img, url:"#", skills:["HTML", "CSS", "JavaScript", "Sass"], text: "This is an explanation of work2. Explain about this work such as when, how I made it." },
+  { id: 3, name: "Work3", img: Work3Img, url:"#", skills:["HTML", "CSS", "Vue.js"], text: "This is an explanation of work3. Explain about this work such as when, how I made it." }
 ]
 
 const Work = () => {
   return (
     <div>
       <Label title="Work" />
-      <div className="project-detail-wrapper">
-        <ProjectDetail img={workObj[0].img} text={workObj[0].text} />
-        <ProjectDetail img={workObj[1].img} text={workObj[1].text} />
-        <ProjectDetail img={workObj[2].img} text={workObj[2].text} />
+      <div className="container">
+        <div className="project-detail-wrapper">
+          {projects.map(project => {
+          return (
+            <ProjectDetail id={project.id} name={project.name} img={project.img} url={project.url} skills={project.skills} text={project.text} />
+          )
+          })}
+        </div>
       </div>
     </div>
   )
