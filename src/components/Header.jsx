5e0916618bd03d6ec6be4663e271297ee1af9244
Hiroki Ueno <rockyueno@gmail.com>
2024-06-20 19:07:13 -0700

    Organize Header codes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/work">Work</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/work">Work</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
