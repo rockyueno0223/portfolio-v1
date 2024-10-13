fe04c7ec394c3b5445064d71cd26acd4cf84f8a1
Hiroki Ueno <rockyueno@gmail.com>
2024-10-12 17:56:31 -0700

    updated project detail style

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const ProjectDetail = ({ project }) => {
-  const ProjectDetailContentClass = ((id) => {
-    let classText = "project-detail-content "
+  const ProjectDetailClass = ((id) => {
+    let classText = "project-detail "
     if (id % 3 === 1) {
       classText += "green"
     } else if (id % 3 === 2) {
@@ -16,13 +16,13 @@ const ProjectDetail = ({ project }) => {
   const skillText = project.skills.join(' / ')
 
   return (
-    <div className='project-detail'>
+    <div className={ProjectDetailClass(project.id)}>
       <div className='project-detail-img'>
         <a href={project.url} target="_blank" rel="noreferrer">
           <img src={`/images/sample/${project.thumbnail}`} alt={project.title} loading='lazy'/>
         </a>
       </div>
-      <div className={ProjectDetailContentClass(project.id)}>
+      <div className='project-detail-content'>
         <h4 className='title'>{project.title}</h4>
         <p className='skills'>{skillText}</p>
         <p className='text'>{project.description}</p>
